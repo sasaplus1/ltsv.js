6c680b72357c4aa84e0eb59f5206f623b7a21eb2
sasaplus1 <sasaplus1@gmail.com>
2013-02-08 17:53:03 +0900

    テストを追加

--- test/test-ltsv_parser.js
+++ test/test-ltsv_parser.js
@@ -21,4 +21,50 @@ suite('ltsvのテスト', function() {
 
   });
 
+  suite('parseLines関数のテスト', function() {
+
+    test('LFで区切られた文字列がパースできること', function() {
+      var result = ltsv.parseLines([
+        'aaa:bbb',
+        'ccc:ddd',
+        'eee:fff'
+      ].join('\n'));
+
+      assert.deepEqual(result[0], {aaa: 'bbb'},
+          'result[0] has {aaa: "bbb"}');
+      assert.deepEqual(result[1], {ccc: 'ddd'},
+          'result[1] has {ccc: "ddd"}');
+      assert.deepEqual(result[2], {eee: 'fff'},
+          'result[2] has {eee: "fff"}');
+    });
+
+    test('CRLFで区切られた文字列がパースできること', function() {
+      var result = ltsv.parseLines([
+        'aaa:bbb',
+        'ccc:ddd',
+        'eee:fff'
+      ].join('\r\n'));
+
+      assert.deepEqual(result[0], {aaa: 'bbb'},
+          'result[0] has {aaa: "bbb"}');
+      assert.deepEqual(result[1], {ccc: 'ddd'},
+          'result[1] has {ccc: "ddd"}');
+      assert.deepEqual(result[2], {eee: 'fff'},
+          'result[2] has {eee: "fff"}');
+    });
+
+    test('CRで区切られた文字列が一行でパースされること', function() {
+      var result = ltsv.parseLines([
+        'aaa:bbb',
+        'ccc:ddd',
+        'eee:fff'
+      ].join('\r'));
+
+      assert.deepEqual(result[0], {
+        aaa: 'bbb\rccc:ddd\reee:fff'
+      }, 'result[0] has {aaa: "bbb\rccc:ddd\reee:fff"}');
+    });
+
+  });
+
 });
