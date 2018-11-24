efa833aed480c9f246d714eb8ce38fe662f531f4
sasaplus1 <sasaplus1@gmail.com>
2018-11-24 15:06:06 +0900

    improved .lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,9 @@ module.exports = {
   linters: {
     '**/*.mjs': 'npm run lint',
     '.*.js': 'npm run lint',
-    'package.json': ['npx fixpack', 'git diff --exit-code --quiet']
+    'package.json': [
+      'npx fixpack',
+      'git diff --exit-code --quiet ./package.json'
+    ]
   }
 };
