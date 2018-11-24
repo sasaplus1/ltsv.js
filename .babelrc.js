5c2569e4bc209170d5d27860cf41ba489266ac3a
sasaplus1 <sasaplus1@gmail.com>
2018-11-24 16:50:26 +0900

    added karma.conf.js

--- .babelrc.js
+++ .babelrc.js
@@ -103,5 +103,45 @@ module.exports = function(api) {
     ];
   }
 
+  if (api.env('karma')) {
+    config.compact = false;
+    // NOTE: fix circular dependencies in core-js
+    // https://github.com/rollup/rollup-plugin-commonjs/issues/284#issuecomment-361085666
+    config.exclude = ['node_modules/core-js/**/*.js'];
+    config.minified = false;
+    config.presets = [
+      [
+        '@babel/preset-env',
+        {
+          debug: true,
+          modules: false,
+          targets: {
+            browsers: ['IE >= 11', 'Android >= 4.4.4']
+          },
+          useBuiltIns: 'usage'
+        }
+      ]
+    ];
+    config.plugins = [
+      [
+        'transform-rename-import',
+        {
+          replacements: [
+            {
+              original: '^assert$',
+              replacement: require.resolve('power-assert/build/power-assert.js')
+            }
+            // {
+            //   original: '(.*)/stream\\.mjs$',
+            //   replacement: '$1/streams.mjs'
+            // }
+          ]
+        }
+      ],
+      // NOTE: this plugin is include in babel-preset-power-assert.
+      'babel-plugin-espower'
+    ];
+  }
+
   return config;
 };
