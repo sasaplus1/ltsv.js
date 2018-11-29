dfff7b672568305076cd70a95b5cd7601bf9fa11
sasaplus1 <sasaplus1@gmail.com>
2018-11-30 00:13:41 +0900

    renamed to browser_stream.mjs from streams.mjs

--- karma.conf.js
+++ karma.conf.js
@@ -52,7 +52,7 @@ module.exports = function(config) {
         watched: true
       },
       {
-        pattern: 'test/**/!(stream|streams).mjs',
+        pattern: 'test/**/!(browser_stream|stream).mjs',
         type: 'js',
         watched: true
       }
