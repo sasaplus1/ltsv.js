630f00b5a4b7519b50e35b4469c986a0f866cf5a
sasaplus1 <sasaplus1@gmail.com>
2019-09-01 00:52:41 +0900

    remove index.mjs

--- index.mjs
+++
@@ -1,9 +0,0 @@
-/**
- * @file entrypoint.
- * @module ltsv
- */
-
-export * from './src/formatter.mjs';
-export * from './src/parser.mjs';
-export * from './src/stream.mjs';
-export * from './src/validator.mjs';
