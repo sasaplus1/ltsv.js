1f2e0073c6f7a594688aaae76049c3a51d067687
sasaplus1 <sasaplus1@gmail.com>
2019-08-31 16:42:38 +0900

    fix .lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,18 +1,16 @@
 module.exports = {
-  linters: {
-    '*.+(js|mjs)': 'npx --no-install eslint --ext .js,.mjs',
-    '*.yml': [
-      'npx --no-install prettier --parser yaml --write',
-      'git diff --exit-code --quiet'
-    ],
-    '!(package|package-lock).json': [
-      'npx --no-install prettier --parser json-stringify --write',
-      'git diff --exit-code --quiet'
-    ],
-    'package.json': [
-      'npx fixpack',
-      'npx --no-install prettier --parser json-stringify --write',
-      'git diff --exit-code --quiet'
-    ]
-  }
+  '*.+(js|mjs)': 'npx --no-install eslint --ext .js,.mjs',
+  '*.yml': [
+    'npx --no-install prettier --parser yaml --write',
+    'git diff --exit-code --quiet'
+  ],
+  '!(package|package-lock).json': [
+    'npx --no-install prettier --parser json-stringify --write',
+    'git diff --exit-code --quiet'
+  ],
+  'package.json': [
+    'npx fixpack',
+    'npx --no-install prettier --parser json-stringify --write',
+    'git diff --exit-code --quiet'
+  ]
 };
