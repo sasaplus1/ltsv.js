1b523d4b6cff39a235a64fab88ed9d9a8c76258a
sasaplus1 <sasaplus1@gmail.com>
2019-09-01 19:21:47 +0900

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,110 +1,123 @@
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript';
 
 import meta from './package.json';
 
-const banner = [
-  '/*!',
-  ' * @license ltsv.js Copyright(c) 2013 sasa+1',
-  ' * https://github.com/sasaplus1/ltsv.js',
-  ' * Released under the MIT license.',
-  ' */'
-].join('\n');
-
-// NOTE: for legacy browsers option
-const babelOptions = {
-  // NOTE: fix circular dependencies in core-js
-  // https://github.com/rollup/rollup-plugin-commonjs/issues/284#issuecomment-361085666
-  exclude: ['node_modules/core-js/**/*.js'],
-  extends: './.babelrc.js',
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        corejs: 3,
-        debug: true,
-        modules: false,
-        targets: {
-          browsers: ['IE >= 11', 'Android >= 4.4.4']
-        },
-        useBuiltIns: 'usage'
-      }
-    ]
-  ]
-};
-
-const nodeResolveOptions = {
-  browser: true,
-  extensions: ['.mjs', '.js'],
-  mainFields: ['module', 'main']
-};
-
-const terserOptions = {
-  output: {
-    preamble: banner
-  }
-};
-
 const config = [];
 
-config.push({
-  input: './index.mjs',
-  output: {
-    banner,
-    file: './umd/ltsv.js',
-    format: 'umd',
-    name: meta.name,
-    sourcemap: true
-  },
-  plugins: [babel(), nodeResolve(nodeResolveOptions), commonjs()]
-});
+if (process.env.build === 'esm') {
+  config.push({
+    input: './entry.ts',
+    output: {
+      file: './dist/esm/index.mjs',
+      format: 'esm'
+    },
+    plugins: [
+      typescript({
+        module: 'ESNext',
+        newLine: 'lf',
+        strict: true,
+        target: 'ESNext'
+      })
+    ]
+  });
+}
 
-config.push({
-  input: './index.mjs',
-  output: {
-    banner,
-    file: './umd/ltsv.min.js',
-    format: 'umd',
-    name: meta.name,
-    sourcemap: true
-  },
-  plugins: [
-    babel(),
-    nodeResolve(nodeResolveOptions),
-    commonjs(),
-    terser(terserOptions)
-  ]
-});
+if (process.env.build === 'umd') {
+  const banner = [
+    '/*!',
+    ' * @license ltsv.js Copyright(c) 2013 sasa+1',
+    ' * https://github.com/sasaplus1/ltsv.js',
+    ' * Released under the MIT license.',
+    ' */'
+  ].join('\n');
 
-config.push({
-  input: './index.mjs',
-  output: {
-    banner,
-    file: './umd/ltsv.legacy.js',
-    format: 'umd',
-    name: meta.name,
-    sourcemap: true
-  },
-  plugins: [babel(babelOptions), nodeResolve(nodeResolveOptions), commonjs()]
-});
-
-config.push({
-  input: './index.mjs',
-  output: {
-    banner,
-    file: './umd/ltsv.legacy.min.js',
-    format: 'umd',
-    name: meta.name,
-    sourcemap: true
-  },
-  plugins: [
-    babel(babelOptions),
-    nodeResolve(nodeResolveOptions),
-    commonjs(),
-    terser(terserOptions)
-  ]
-});
+  config.push(
+    {
+      input: './entry.ts',
+      output: {
+        banner,
+        file: `./dist/umd/${meta.name}.js`,
+        format: 'umd',
+        name: meta.name,
+        sourcemap: true
+      },
+      plugins: [
+        typescript({
+          newLine: 'lf',
+          strict: true,
+          sourceMap: true,
+          target: 'ESNext'
+        })
+      ]
+    },
+    {
+      input: './entry.ts',
+      output: {
+        banner,
+        file: `./dist/umd/${meta.name}.min.js`,
+        format: 'umd',
+        name: meta.name,
+        sourcemap: true
+      },
+      plugins: [
+        typescript({
+          newLine: 'lf',
+          strict: true,
+          sourceMap: true,
+          target: 'ESNext'
+        }),
+        terser({
+          output: {
+            preamble: banner
+          },
+          sourcemap: true
+        })
+      ]
+    },
+    {
+      input: './entry.ts',
+      output: {
+        banner,
+        file: `./dist/umd/${meta.name}.legacy.js`,
+        format: 'umd',
+        name: meta.name,
+        sourcemap: true
+      },
+      plugins: [
+        typescript({
+          newLine: 'lf',
+          strict: true,
+          sourceMap: true,
+          target: 'ES5'
+        })
+      ]
+    },
+    {
+      input: './entry.ts',
+      output: {
+        banner,
+        file: `./dist/umd/${meta.name}.legacy.min.js`,
+        format: 'umd',
+        name: meta.name,
+        sourcemap: true
+      },
+      plugins: [
+        typescript({
+          newLine: 'lf',
+          strict: true,
+          sourceMap: true,
+          target: 'ES5'
+        }),
+        terser({
+          output: {
+            preamble: banner
+          },
+          sourcemap: true
+        })
+      ]
+    }
+  );
+}
 
 export default config;
