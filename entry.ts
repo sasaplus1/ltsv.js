cd95ac9a3e365bfb25487d4cd6d269283a9d48f1
sasaplus1 <sasaplus1@gmail.com>
2019-09-01 18:47:35 +0900

    rename index.*.ts

--- entry.ts
+++ entry.ts
@@ -1,3 +1,5 @@
+/**
+ * @file for ES Modules and UMD
+ */
 export * from './src/index';
-
 export * from './src/whatwg_stream';
--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+/**
+ * @file for CommonJS
+ */
 export * from './src/index';
-
 export * from './src/nodejs_stream';
