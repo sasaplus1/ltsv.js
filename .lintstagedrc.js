88c7a7834359bdaa4a4cc69e4002416c238edbc9
sasaplus1 <sasaplus1@gmail.com>
2021-09-11 23:49:17 +0900

    improve lint-staged settings

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-  '*.(js|ts)': 'npx eslint --cache --ext .js,.ts',
-  '*.yml': [
-    'npx prettier --parser yaml --write',
-    'git diff --exit-code --quiet'
-  ],
+  '*.+(js|ts)': ['eslint --cache', 'prettier --check'],
+  '*.+(md|yml)': ['prettier --check'],
+  '*.ts'() {
+    return 'tsc --noEmit';
+  },
+  '!(package|package-lock).json': 'prettier --check --parser json-stringify',
   'package.json': [
-    'npx fixpack',
-    'npx prettier --parser json-stringify --write',
-    'git diff --exit-code --quiet'
+    'npx fixpack --dryRun',
+    'prettier --check --parser json-stringify'
   ],
-  'package-lock.json': 'node -e "process.exit(1);"'
+  'package-lock.json': 'node -e "process.exitCode = 1;"'
 };
