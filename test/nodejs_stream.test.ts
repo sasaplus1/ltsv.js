a5df2ae150c9d0c9cc96b1bfec8c876f610a1d87
sasaplus1 <sasaplus1@gmail.com>
2025-03-23 15:07:23 +0900

    promisify

--- test/nodejs_stream.test.ts
+++ test/nodejs_stream.test.ts
@@ -10,62 +10,9 @@ const __dirname = path.dirname(__filename);
 
 describe('nodejs_stream', function () {
   describe('#write() and #end()', function () {
-    it('should convert to JSON from LTSV', function (done) {
-      const stream = createLtsvToJsonStream();
-      const buffer: string[] = [];
-
-      stream.on('readable', function () {
-        for (let buf = stream.read(); buf != null; buf = stream.read()) {
-          buffer.push(buf);
-        }
-      });
-      stream.once('end', function () {
-        assert.deepStrictEqual(buffer, [
-          '{"label1":"value1","label2":"value2"}',
-          '{"label3":"value3","label4":"value4"}'
-        ]);
-        done();
-      });
-
-      stream.write('label1:value1\tlabel2:value2');
-      stream.end('\nlabel3:value3\tlabel4:value4');
-    });
-
-    it('should convert to JSON if LTSV has empty line of end', function (done) {
-      const stream = createLtsvToJsonStream();
-      const buffer: string[] = [];
-
-      stream.on('readable', function () {
-        for (let buf = stream.read(); buf != null; buf = stream.read()) {
-          buffer.push(buf);
-        }
-      });
-      stream.once('end', function () {
-        assert.deepStrictEqual(buffer, [
-          '{"label1":"value1","label2":"value2"}',
-          '{"label3":"value3","label4":"value4"}'
-        ]);
-        done();
-      });
-
-      stream.write('label1:value1\tlabel2:value2');
-      stream.end('\nlabel3:value3\tlabel4:value4\n');
-    });
-
-    it('should throw error if illegal LTSV', function (done) {
-      const stream = createLtsvToJsonStream();
-
-      stream.once('error', function (err) {
-        assert(err !== null);
-        done();
-      });
-
-      stream.end('\t');
-    });
-
-    describe('objectMode mode', function () {
-      it('should convert to Object from LTSV', function (done) {
-        const stream = createLtsvToJsonStream({ objectMode: true });
+    it('should convert to JSON from LTSV', function () {
+      return new Promise<void>((resolve) => {
+        const stream = createLtsvToJsonStream();
         const buffer: string[] = [];
 
         stream.on('readable', function () {
@@ -75,10 +22,10 @@ describe('nodejs_stream', function () {
         });
         stream.once('end', function () {
           assert.deepStrictEqual(buffer, [
-            { label1: 'value1', label2: 'value2' },
-            { label3: 'value3', label4: 'value4' }
+            '{"label1":"value1","label2":"value2"}',
+            '{"label3":"value3","label4":"value4"}'
           ]);
-          done();
+          resolve();
         });
 
         stream.write('label1:value1\tlabel2:value2');
@@ -86,88 +33,157 @@ describe('nodejs_stream', function () {
       });
     });
 
-    describe('isStrict mode', function () {
-      it('should throw error if LTSV has unasserted character', function (done) {
-        const stream = createLtsvToJsonStream({ strict: true });
+    it('should convert to JSON if LTSV has empty line of end', function () {
+      return new Promise<void>((resolve) => {
+        const stream = createLtsvToJsonStream();
+        const buffer: string[] = [];
+
+        stream.on('readable', function () {
+          for (let buf = stream.read(); buf != null; buf = stream.read()) {
+            buffer.push(buf);
+          }
+        });
+        stream.once('end', function () {
+          assert.deepStrictEqual(buffer, [
+            '{"label1":"value1","label2":"value2"}',
+            '{"label3":"value3","label4":"value4"}'
+          ]);
+          resolve();
+        });
+
+        stream.write('label1:value1\tlabel2:value2');
+        stream.end('\nlabel3:value3\tlabel4:value4\n');
+      });
+    });
+
+    it('should throw error if illegal LTSV', function () {
+      return new Promise<void>((resolve) => {
+        const stream = createLtsvToJsonStream();
 
         stream.once('error', function (err) {
           assert(err !== null);
-          done();
+          resolve();
         });
 
-        stream.end('+:');
+        stream.end('\t');
+      });
+    });
+
+    describe('objectMode mode', function () {
+      it('should convert to Object from LTSV', function () {
+        return new Promise<void>((resolve) => {
+          const stream = createLtsvToJsonStream({ objectMode: true });
+          const buffer: string[] = [];
+
+          stream.on('readable', function () {
+            for (let buf = stream.read(); buf != null; buf = stream.read()) {
+              buffer.push(buf);
+            }
+          });
+          stream.once('end', function () {
+            assert.deepStrictEqual(buffer, [
+              { label1: 'value1', label2: 'value2' },
+              { label3: 'value3', label4: 'value4' }
+            ]);
+            resolve();
+          });
+
+          stream.write('label1:value1\tlabel2:value2');
+          stream.end('\nlabel3:value3\tlabel4:value4');
+        });
+      });
+    });
+
+    describe('isStrict mode', function () {
+      it('should throw error if LTSV has unasserted character', function () {
+        return new Promise<void>((resolve) => {
+          const stream = createLtsvToJsonStream({ strict: true });
+
+          stream.once('error', function (err) {
+            assert(err !== null);
+            resolve();
+          });
+
+          stream.end('+:');
+        });
       });
     });
   });
 
   describe('#pipe()', function () {
-    it('should convert LTSV log of 1 line', function (done) {
-      const stream = createLtsvToJsonStream();
-      const buffer: string[] = [];
+    it('should convert LTSV log of 1 line', function () {
+      return new Promise<void>((resolve) => {
+        const stream = createLtsvToJsonStream();
+        const buffer: string[] = [];
 
-      if (stream === null) {
-        return assert.fail();
-      }
-
-      stream.on('readable', function () {
         if (stream === null) {
-          return;
+          return assert.fail();
         }
 
-        for (let buf = stream.read(); buf != null; buf = stream.read()) {
-          buffer.push(buf);
-        }
-      });
-      stream.once('end', function () {
-        assert.deepStrictEqual(buffer, [
-          '{"aaa":"bbb","ccc":"ddd","eee":"fff"}'
-        ]);
-        done();
-      });
+        stream.on('readable', function () {
+          if (stream === null) {
+            return;
+          }
 
-      fs.createReadStream(path.join(__dirname, './log/valid-1.ltsv')).pipe(
-        stream
-      );
+          for (let buf = stream.read(); buf != null; buf = stream.read()) {
+            buffer.push(buf);
+          }
+        });
+        stream.once('end', function () {
+          assert.deepStrictEqual(buffer, [
+            '{"aaa":"bbb","ccc":"ddd","eee":"fff"}'
+          ]);
+          resolve();
+        });
+
+        fs.createReadStream(path.join(__dirname, './log/valid-1.ltsv')).pipe(
+          stream
+        );
+      });
     });
 
-    it('should convert LTSV log of 3 line', function (done) {
-      const stream = createLtsvToJsonStream();
-      const buffer: string[] = [];
+    it('should convert LTSV log of 3 line', function () {
+      return new Promise<void>((resolve) => {
+        const stream = createLtsvToJsonStream();
+        const buffer: string[] = [];
 
-      stream.on('readable', function () {
-        if (stream === null) {
-          return;
-        }
-        for (let buf = stream.read(); buf != null; buf = stream.read()) {
-          buffer.push(buf);
-        }
-      });
-      stream.once('end', function () {
-        assert.deepStrictEqual(buffer, [
-          '{"aaa":"bbb","ccc":"ddd","eee":"fff"}',
-          '{"aaa":"bbb","ccc":"ddd","eee":"fff"}',
-          '{"aaa":"bbb","ccc":"ddd","eee":"fff"}'
-        ]);
-        done();
-      });
+        stream.on('readable', function () {
+          if (stream === null) {
+            return;
+          }
+          for (let buf = stream.read(); buf != null; buf = stream.read()) {
+            buffer.push(buf);
+          }
+        });
+        stream.once('end', function () {
+          assert.deepStrictEqual(buffer, [
+            '{"aaa":"bbb","ccc":"ddd","eee":"fff"}',
+            '{"aaa":"bbb","ccc":"ddd","eee":"fff"}',
+            '{"aaa":"bbb","ccc":"ddd","eee":"fff"}'
+          ]);
+          resolve();
+        });
 
-      fs.createReadStream(path.join(__dirname, './log/valid-3.ltsv')).pipe(
-        stream
-      );
+        fs.createReadStream(path.join(__dirname, './log/valid-3.ltsv')).pipe(
+          stream
+        );
+      });
     });
 
-    it('should throw error if invalid LTSV log', function (done) {
-      const stream = createLtsvToJsonStream();
+    it('should throw error if invalid LTSV log', function () {
+      return new Promise<void>((resolve) => {
+        const stream = createLtsvToJsonStream();
 
-      // XXX: why fail when use stream.once?
-      stream.on('error', function (err) {
-        assert(err !== null);
-        done();
+        // XXX: why fail when use stream.once?
+        stream.on('error', function (err) {
+          assert(err !== null);
+          resolve();
+        });
+
+        fs.createReadStream(path.join(__dirname, './log/invalid.ltsv')).pipe(
+          stream
+        );
       });
-
-      fs.createReadStream(path.join(__dirname, './log/invalid.ltsv')).pipe(
-        stream
-      );
     });
   });
 });
